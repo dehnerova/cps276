cbe0904f8e29ac3da0c27f4e977a03fb640cbfd9
Alexandra Dehnerova <adehnerova@wccnet.edu>
2021-10-11 17:42:32 -0400

    assign3 finished

--- assignments/assignment3/Test_Calculator.php
+++ assignments/assignment3/Test_Calculator.php
@@ -21,8 +21,8 @@ $calculator = new Calculator();
     echo $calculator->calc("/", 10, 0); //will output Cannot divide by zero
     echo $calculator->calc("*", 10, 2); //will output The product of the numbers is 20
     echo $calculator->calc("/", 10, 2); //will output The division of the numbers is 5
-    echo $calculator->calc("-", 10, 2); //will output The difference of the numbers is 8 echo $Calculator->calc("+", 10, 2); //will output The sum of the numbers is 12
-    echo $calculator->calc("*", 10); //will output You must enter a string and two numbers echo $Calculator->calc(10); //will output You must enter a string and two numbers
+    echo $calculator->calc("-", 10, 2); //will output The difference of the numbers is 8 
+    echo $calculator->calc("*", 10); //will output You must enter a string and two numbers 
     echo $calculator->calc(10); //will output You must enter a string and two numbers
     ?>
 </body>
