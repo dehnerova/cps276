bc265308a7179a6f6cdeef6f169388bcf98e610a
Alexandra Dehnerova <adehnerova@wccnet.edu>
2021-10-30 16:15:01 -0400

    test

---
+++ connection.sql
@@ -0,0 +1,12 @@
+try {
+# MS SQL Server and Sybase with PDO_DBLIB
+$DBH = new PDO("mssql:host=$host;dbname=$dbname, $user, $pass");
+$DBH = new PDO("sybase:host=$host;dbname=$dbname, $user, $pass");
+# MySQL with PDO_MYSQL
+$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+# SQLite Database
+$DBH = new PDO("sqlite:my/database/path/database.db");
+}
+catch(PDOException $e) {
+echo $e->getMessage();
+}
